7ff168f23edc31328a4601e172a5bd0c45f8fdc5
SyedaZannatulTushi <syedazannatultushi524@gmail.com>
2025-09-26 02:01:00 +0600

    added footer section

---
+++ home.js
@@ -0,0 +1,33 @@
+const sareeBtn = document.getElementById('saree-btn');
+sareeBtn.addEventListener('click',function(){
+ 'saree-page.html'
+ window,location.href = ' saree-page.html';
+})
+
+const threepiceBtn = document.getElementById('three-pice-btn');
+threepiceBtn.addEventListener('click',function(){
+ 'three-pice.html'
+ window,location.href =' three-pice.html'
+})
+
+const westernBtn = document.getElementById('western-btn');
+westernBtn.addEventListener('click',function(){
+    'western.html'
+    window.location.href = 'western.html';
+})
+
+const makeupBtn = document.getElementById('makeup-btn');
+makeupBtn.addEventListener('click',function(){
+    'makeup.html'
+    window.location.href = 'makeup.html';
+})
+const showBtn = document.getElementById('show-btn');
+showBtn.addEventListener('click',function(){
+    'show-page.html'
+    window.location.href = 'show-page.html';
+})
+const jewelleryBtn = document.getElementById('jewellery-btn');
+jewelleryBtn.addEventListener('click',function(){
+    'jewellery-page.html'
+    window.location.href = 'jewellery-page.html';
+})
